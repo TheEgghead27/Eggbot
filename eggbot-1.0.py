4b4eea0d2d60f7073b5041b6db8e3f5542ede375
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2020-05-17 16:01:26 -0400

    add case_insenstive clause

--- eggbot-1.0.py
+++ eggbot-1.0.py
@@ -20,7 +20,7 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-bot = commands.Bot(command_prefix='e!', description="e!help")
+bot = commands.Bot(command_prefix='e!', case_insensitive=True, description="e!help")
 bot.remove_command("help")
 
 
