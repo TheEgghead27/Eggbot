fdb5b607dd4e1d37080551ac472db3fa961dca96
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2020-06-09 18:38:03 -0400

    clean up files after conversion

--- convert.py
+++ convert.py
@@ -1,13 +1,18 @@
 import json
+import os
 
 file = 'no file'
 try:
     file = "host.txt"
     with open(file, 'r') as hosts:
         hosts = hosts.read().split("\n")
+    if os.path.exists(file):
+        os.remove(file)
     file = 'token.txt'
     with open(file, 'r') as token:
         token = token.read()
+    if os.path.exists(file):
+        os.remove(file)
     data = {"hosts": hosts, "token": token}
     with open("config.json", "w") as config:
         json.dump(data, config)
