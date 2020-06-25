2ba8ed7ab25f278b0cca7b38ba465c0341b8fd93
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2020-06-25 19:31:01 -0400

    85% done with economy

--- eggbot.py
+++ eggbot.py
@@ -58,12 +58,13 @@ roles = {}
 colors = {}
 roleEmbeds = {}
 stonks = {}
+warehouse = {}
 blacklist = []
 
 
 def load(exclude):
     """read files for data"""
-    global hosts, token, Bee, kirilist, eggs, eggTrigger, spic, simp, ohno, roles, colors, blacklist, stonks
+    global hosts, token, Bee, kirilist, eggs, eggTrigger, spic, simp, ohno, roles, colors, blacklist, stonks, warehouse
     # read all the files for variables
     file = "No file"
     try:
@@ -235,11 +236,11 @@ async def on_message(message):
     if botSafeguard and message.author.bot and not message.author.id == bot.user.id:
         return
     else:
-        oval = random.randrange(0, 5)
-        if oval <= 4:
+        oval = random.randrange(0, 6)
+        if oval <= 2:
             pass
         else:
-            oval = random.randrange(1, 5)
+            oval = random.randrange(1, 3)
             try:
                 stonks["users"][str(message.author.id)][str(message.guild.id)] += oval
                 if stonks["users"][str(message.author.id)]["notif"] == "True":
@@ -661,12 +662,8 @@ async def say(ctx, *args):
         except ValueError:
             channel = ctx.channel
         finally:
-            echo = ""
-            while arghs:
-                echo = echo + " " + arghs[0]
-                del arghs[0]
+            echo = joinArgs(arghs)
             if echo == "" or echo is None:
-                print("a")
                 if not ctx.message.content[len(prefix) + 4:] == "":
                     await ctx.send(ctx.message.content[len(prefix) + 4:])
                 else:
@@ -678,6 +675,14 @@ async def say(ctx, *args):
                     await ctx.send(echo)
     else:
         return
+
+
+def joinArgs(arghs):
+    echo = ""
+    while arghs:
+        echo = echo + " " + arghs[0]
+        del arghs[0]
+    return echo
 
 
 @bot.command()
@@ -936,7 +941,8 @@ def write():
     with open("roles.json", "w") as j:
         json.dump(roles, j, encoding="utf-8")
     with open("stonks.json", "w") as j:
-        json.dump(stonks, j, encoding="utf-8")
+        dick = {"moneys": stonks, "amazon": warehouse}
+        json.dump(dick, j, encoding="utf-8")
 
 
 @bot.command()
@@ -946,10 +952,16 @@ async def economyHelp(ctx):
     emb.add_field(name="Server Eggs", value="Eggs rewarded for interactions in the server.", inline=False)
     emb.add_field(name="e!fridge", value="Shows the number of global and server eggs you own.", inline=False)
     emb.add_field(name="e!bank", value="Shows the current number of server eggs donated to the server.", inline=False)
+    emb.add_field(name="e!goals", value="Displays the server goals. One can contribute to the funding of the goals by "
+                                        "using e!donate.", inline=False)
     # emb.add_field(name="e!setGoal", value="Sets a fundraising goal displayed in e!bank. "
     #                                       "Use e!accept to deduct the balance from the bank.")
     emb.add_field(name="e!donate {number}", value="Donates the specified number of eggs to the server.", inline=False)
     emb.add_field(name="e!notifs {on/off}", value="Toggles notifications for eggs earned.", inline=False)
+    emb.add_field(name="e!setGoal {cost} {name}", value="Sets a server goal. (admin only)", inline=False)
+    emb.add_field(name="e!deleteGoal {name}", value="Deletes a server goal. (admin only)", inline=False)
+    emb.add_field(name="e!addEggs {number}", value="Adds eggs to the server bank. (admin only)", inline=False)
+    emb.add_field(name="e!removeEggs {number}", value="Removes eggs from the server bank. (admin only)", inline=False)
     await ctx.send(embed=emb)
 
 
@@ -1003,13 +1015,43 @@ async def bank(ctx):
         emb = discord.Embed(title="{s} Bank Balance:".format(s=str(ctx.guild)), description="0", color=0xfefefe)
         await ctx.send(embed=emb)
     except AttributeError:
-        pass
+        await ctx.send("Bruh, this isn't a server!?!")
+
+
+@bot.command()
+async def goals(ctx):
+    try:
+        emb = discord.Embed(title="{s} Server Goals:".format(s=str(ctx.guild)), color=0x00ff55)
+        a = warehouse[str(ctx.guild.id)]
+        if len(a) > 0:
+            i = len(a) // 2
+            b = 0
+            c = 1
+            while i > 0:
+                if a[c] == 1:
+                    v = "1 egg"
+                else:
+                    v = "{e} eggs".format(e=str(a[c]))
+                emb.add_field(name=a[b], value=v, inline=False)
+                b += 2
+                c += 2
+                i -= 1
+        else:
+            emb.add_field(name="None", value="There are no goals set in this server.")
+        await ctx.send(embed=emb)
+    except AttributeError:
+        await ctx.send("Bruh, this isn't a server!?!")
+    except KeyError:
+        warehouse[str(ctx.guild.id)] = []
+        emb = discord.Embed(title="{s} Server Goals:".format(s=str(ctx.guild)), color=0x00ff55)
+        emb.add_field(name="None", value="There are no goals set in this server.")
+        await ctx.send(embed=emb)
 
 
 @bot.command()
 async def donate(ctx, arg1):
     try:
-        wallet = stonks["users"][str(ctx.author.id)]
+        wallet = stonks["users"][str(ctx.author.id)][str(ctx.guild.id)]
         arg1 = int(arg1)
     except KeyError:
         await ctx.send("Don't be cheeky, you don't have any eggs to donate!")
@@ -1018,26 +1060,115 @@ async def donate(ctx, arg1):
         await ctx.send("Don't be cheeky, this isn't even a server!")
         return
     try:
-        if wallet[str(ctx.guild.id)] >= arg1:
+        if wallet < arg1:
+            # stonks["users"][str(ctx.author.id)][str(ctx.guild.id)] = 0
             await ctx.send("Don't be cheeky, you don't have that many eggs to donate!")
             return
         stonks["servers"][str(ctx.guild.id)] += arg1
     except KeyError:
         stonks["servers"][str(ctx.guild.id)] = arg1
-    wallet[str(ctx.guild.id)] -= arg1
-    emb = discord.Embed(title="Donated {e} eggs to {s} Server".format(e=str(arg1), s=str(ctx.guild)),
+    wallet -= arg1
+    emb = discord.Embed(title="👍 Donated {e} eggs to {s} Server".format(e=str(arg1), s=str(ctx.guild)),
                         color=0x00ff55)
     await ctx.send(embed=emb)
 
 
 @bot.command()
-async def setGoal(ctx, args):
-    if ctx.author.guild_permissions.administrator:
-        cost = int(args[0])
+async def setGoal(ctx, *args):
+    try:
+        if ctx.author.guild_permissions.administrator:
+            try:
+                a = warehouse[str(ctx.guild.id)]
+            except KeyError:
+                a = warehouse[str(ctx.guild.id)] = []
+            try:
+                if len(a) <= 8:
+                    args = list(args)
+                    cost = int(args[0])
+                    if cost <= 0:
+                        await ctx.send("dude what the heck how do you buy something for 0 or less money?!? the heck?")
+                        return
+                    del args[0]
+                    name = joinArgs(arghs=args)
+                    name = name.strip(' ')
+                    if len(name) == 0:
+                        raise IndexError
+                    a.append(name)
+                    a.append(cost)
+                    await ctx.send("Set a goal of `{c}` egg(s) for `{g}`.".format(c=str(cost), g=name))
+                else:
+                    await ctx.send("You have reached the limit of goals you can set. You can only have 5 goals.")
+            except (IndexError, ValueError):
+                await ctx.send("You didn't provide the correct syntax. The syntax is `e!setGoals [cost] [name]`.")
+        else:
+            await ctx.send("Bruh, you can't do that!")
+    except AttributeError:
+        await ctx.send("Bruh, this isn't a server!?!")
 
-        pass
-    else:
-        await ctx.send("Bruh, you can't do that!")
+
+@bot.command()
+async def deleteGoal(ctx, *args):
+    try:
+        if ctx.author.guild_permissions.administrator:
+            try:
+                if len(warehouse[str(ctx.guild.id)]) == 0:
+                    await ctx.send("You don't have any goals to delete???????????")
+                else:
+                    args = list(args)
+                    name = joinArgs(arghs=args)
+                    name = name.strip(' ')
+                    a = 0
+                    for _ in warehouse[str(ctx.guild.id)]:
+                        if name == warehouse[str(ctx.guild.id)][a]:
+                            del warehouse[str(ctx.guild.id)][a], warehouse[str(ctx.guild.id)][a]
+                            await ctx.send("Deleted the `{g}` goal.".format(g=name))
+                            return
+                        else:
+                            a += 1
+                    await ctx.send("There is no goal called `{g}` to delete.".format(g=name))
+            except (IndexError, ValueError):
+                await ctx.send("You didn't provide the correct syntax. The syntax is `e!deleteGoals [name]`.")
+        else:
+            await ctx.send("Bruh, you can't do that!")
+    except AttributeError:
+        await ctx.send("Bruh, this isn't a server!?!")
+
+
+@bot.command()
+async def addEggs(ctx, arg1):
+    try:
+        if ctx.author.guild_permissions.administrator:
+            arg1 = int(arg1)
+            try:
+                stonks["servers"][str(ctx.guild.id)] += arg1
+            except KeyError:
+                stonks["servers"][str(ctx.guild.id)] = arg1
+            await ctx.send("Added {e} eggs to the server's egg bank.".format(e=str(arg1)))
+        else:
+            await ctx.send("Bruh, you can't do that!")
+    except AttributeError:
+        await ctx.send("Bruh, this isn't a server!?!")
+
+
+@bot.command()
+async def removeEggs(ctx, arg1):
+    try:
+        if ctx.author.guild_permissions.administrator:
+            arg1 = int(arg1)
+            try:
+                if arg1 <= stonks["servers"][str(ctx.guild.id)]:
+                    stonks["servers"][str(ctx.guild.id)] -= arg1
+                    await ctx.send("Removed {e} eggs from the server's egg bank.".format(e=str(arg1)))
+                else:
+                    stonks["servers"][str(ctx.guild.id)] = 0
+                    await ctx.send("Emptied the server's egg bank.")
+            except KeyError:
+                stonks["servers"][str(ctx.guild.id)] = 0
+                await ctx.send("The server bank doesn't have any eggs to remove?")
+        else:
+            await ctx.send("Bruh, you can't do that!")
+    except AttributeError:
+        await ctx.send("Bruh, this isn't a server!?!")
 
 
 @bot.event
@@ -1046,9 +1177,10 @@ async def on_command(ctx):
     try:
         args = ctx.message.content.split(' ')[0]
         args = args[2:]
-        if args in ["help", "invite", "server", "github", "admins", "test_args", "fridge", "bank", "notifs", "save",
+        if args in ("help", "invite", "server", "github", "admins", "test_args", "fridge", "bank", "notifs", "save",
                     "say", "rolegiver", "addroles", "backuproles", "save", "reloadroles", 'log', 'debug', 'spam',
-                    'botspam', 'shutdown', 'print_emoji', 'economyhelp']:
+                    'botspam', 'shutdown', 'print_emoji', 'economyhelp', 'donate', 'goals', 'setgoal', 'deletegoal',
+                    'addeggs', 'removeeggs'):
             pass
         else:
             oval = random.randrange(0, 10)
