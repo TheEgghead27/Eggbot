1c5ca74cb83022ea1324bbc1ac2d9b4a601c75af
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2020-07-16 10:34:22 -0400

    bug fixes

--- eggbot.py
+++ eggbot.py
@@ -899,13 +899,18 @@ async def reloadRoles(ctx):
         try:
             with open("roles.json.bak", "r+") as roles:
                 roles = json.load(roles)
+                joinRoles = roles["join"]
+                roles = roles["reactions"]
         except FileNotFoundError:
             await ctx.send("There is no backup, it is highly recommended that you use `e!backupRoles` to create one.")
             with open("roles.json", "r+") as roles:
                 roles = json.load(roles)
+                joinRoles = roles["join"]
+                roles = roles["reactions"]
         await asyncio.sleep(1)
         with open("roles.json", "w") as J:
-            json.dump(roles, J, encoding="utf-8")
+            dick = {"reactions": roles, "join": joinRoles}
+            json.dump(dick, J, encoding="utf-8")
         await ctx.send("Restored role database from backup.")
 
 
@@ -1340,30 +1345,31 @@ async def on_raw_reaction_add(payload):
         roleData = roles[str(payload.message_id)]
         if emoji in roleData:
             roleData = roleData[emoji]
+            react_guild = bot.get_guild(payload.guild_id)
+            react_user = react_guild.get_member(payload.user_id)
+            if react_user.id == bot.user.id:  # don't let the bot count its own reactions
+                return
+            else:
+                role = react_guild.get_role(roleData['role'])
+                try:
+                    await react_user.add_roles(role)  # edit role
+                    if 'addMessage' in roleData:
+                        mess = roleData['addMessage']
+                    else:
+                        mess = "You now have the @{} role.".format(role.name)
+                    emb = discord.Embed(title="Role Confirmed!", description=mess, color=0x0ac845)
+                    await react_user.send(embed=emb)
+                except discord.Forbidden:
+                    emb = discord.Embed(title="Error: Missing Permissions", description="I don't have permission to give you "
+                                                                                        "that role! Please notify a moderator "
+                                                                                        "so I can get the `Manage Roles` "
+                                                                                        "permission!", color=0xbc1a00)
+                    await react_user.send(embed=emb)
         else:
             return
     else:
         return
-    react_guild = bot.get_guild(payload.guild_id)
-    react_user = react_guild.get_member(payload.user_id)
-    if react_user.id == bot.user.id:  # don't let the bot count its own reactions
-        return
-    else:
-        role = react_guild.get_role(roleData['role'])
-        try:
-            await react_user.add_roles(role)  # edit role
-            if 'addMessage' in roleData:
-                mess = roleData['addMessage']
-            else:
-                mess = "You now have the @{} role.".format(role.name)
-            emb = discord.Embed(title="Role Confirmed!", description=mess, color=0x0ac845)
-            await react_user.send(embed=emb)
-        except discord.Forbidden:
-            emb = discord.Embed(title="Error: Missing Permissions", description="I don't have permission to give you "
-                                                                                "that role! Please notify a moderator "
-                                                                                "so I can get the `Manage Roles` "
-                                                                                "permission!", color=0xbc1a00)
-            await react_user.send(embed=emb)
+    
 
 
 @bot.event
